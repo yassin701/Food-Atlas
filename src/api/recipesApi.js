7d8a47b17e4c96450d8b413e9346280dfd1d15c7
Osama-Amg <amghar.oussama8.57@gmail.com>
2025-11-25 14:43:11 +0100

    ffix : branch error

--- src/api/recipesApi.js
+++ src/api/recipesApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/"
+const API_URL = "http://localhost:3001/recipes"
 
 export const getRecipes = async () => {
     const response = await axios.get(API_URL);
