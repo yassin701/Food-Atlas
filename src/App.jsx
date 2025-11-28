68592b9744bef21c9d444c59abc406fc2c4668c8
ahlam201601 <elhadriahlam9@gmail.com>
2025-11-28 10:48:59 +0100

    page admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";          // ممكن تتركو فالبداية فارغ
-import Recipes from "./pages/Recipes";    // ممكن زملائك يصاوبوها من بعد
+import Home from "./pages/Home";          
+import Recipes from "./pages/Recipes";    
 import Details from "./pages/Details";
 import AddRecipe from "./pages/AddRecipes";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/recipes" element={<Recipes />} /> 
         <Route path="/recipes/:id" element={<Details />} /> 
         <Route path="/admin/add" element={<AddRecipe />} />
+        <Route path="/admin"  element={<Admin/>} />
     
       </Routes>
     </Router>
